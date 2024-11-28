5fbd02c461e028c8bc622228ad5a0bd9bd161ef4
Aziz Muradovich <muradovich.a22@gmail.com>
2024-11-28 14:53:18 +0500

    feat: add dynamic input

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -6,14 +6,41 @@
       </Tooltip>
     </div>
     <div>
-      <FormInput v-model="text" />
+      <FormInput v-model="text" label="Miqdor" class="mb-2" type="number" />
+      <div class="flex items-center gap-3">
+        <div
+          class="w-fit px-2 py-1 rounded bg-white cursor-pointer text-dark text-sm box-content"
+          :class="
+            text === i
+              ? 'bg-orange-400/10 border border-orange-400 text-dark'
+              : 'bg-white'
+          "
+          v-for="(i, idx) in donatValues"
+          :key="idx"
+          @click="text = i"
+        >
+          {{ i }}
+        </div>
+      </div>
       <pre>{{ text }}</pre>
     </div>
-    <div>
-      <CustomInput v-model="text3" />
-      <pre>{{ text3 }}</pre>
+    <div class="flex items-center gap-3">
+      <div
+        class="w-fit px-2 py-1 rounded bg-white cursor-pointer text-dark text-sm box-content"
+        :class="
+          provider === i
+            ? 'bg-orange-400/10 border border-orange-400 text-dark'
+            : 'bg-white'
+        "
+        v-for="(i, idx) in providers"
+        :key="idx"
+        @click="provider = i"
+      >
+        {{ i }}
+      </div>
     </div>
-    <div class="py-5">
+    <pre>{{ provider }}</pre>
+    <div class="py-5" v-if="false">
       <Radio v-model="radio" name="myRadioGroup" value="1" label="Option 2" />
       <pre>value: {{ radio }}</pre>
     </div>
@@ -21,13 +48,15 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { reactive, ref } from 'vue'
 
-import CustomInput from '@/components/Form/CustomInput.vue'
 import FormInput from '@/components/Form/Input.vue'
 import Radio from '@/components/Form/Radio.vue'
 import Tooltip from '@/components/Tooltip.vue'
-const text = ref('')
-const text3 = ref('')
+const text = ref(0)
+const provider = ref('')
 const radio = ref()
+
+const donatValues = reactive([1000, 5000, 10000])
+const providers = reactive(['payme', 'click', 'visa'])
 </script>
